3e3877397b20fb169abc9f6f1673922ce996048d
chenxiubi <15622715184@163.com>
2020-09-02 21:42:25 +0800

    2020-09-02

--- WebApplication.knowledge/IMyDependency.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace WebApplication.knowledge
-{
-    public interface IMyDependency
-    {
-        Task<string> WriteMessage(string message);
-    }
-}
--- WebApplication.knowledge/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApplication.knowledge
-{
-    public class User
-    {
-        public string Name { get; set; }
-    }
-}
